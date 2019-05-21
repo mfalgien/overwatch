f384e52461772af7f87364611b068a1f67ae7a68
Michael Falgien <mfalgien@gmail.com>
2019-05-21 15:25:48 -0700

    creating call to overwatch api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import overwatch from '../apis/overwatch';
 
 class App extends React.Component {
+  
+  onNameSubmit = async name => {
+   const response = await overwatch.get(`/pc/us/${name}/complete`);
+   console.log(response);
+  }
+
   render(){
-    return <div><SearchBar /> </div>;
+    return <div><SearchBar onFormSubmit={this.onNameSubmit}/> </div>;
   } 
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,22 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { username: "" };
 
+  onInputChange = event => {
+    this.setState({username: event.target.value});
+  }
+
+  onFormSubmit = event =>{
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.username);
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form onSubmit="" className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
         <div className="field">
           <label>BattleTag</label>
-          <input type="text" />
+          <input type="text" value={this.state.username} onChange={this.onInputChange} />
         </div>
         </form>
       </div>
